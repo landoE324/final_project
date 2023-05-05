e8c19ee287d591ef2b457bbb728704efb3230b45
CTC CCP 4 <ctcccp4@CTCs-Air.cape.k12.mo.us>
2023-05-05 09:51:27 -0500

    javascript

--- script.js
+++ script.js
@@ -1,18 +1,45 @@
-var addItem = document.getElementsByClassName("buynow");
+window.addEventListener("load",function() {
+    calcCart();
+    var cart = document.forms.cart;
+    var shippingOptions = document.querySelectorAll('input[name="shipping"]');
+    for (var i = 0; i <= shippingOptions.length; i++) {
+        shippingOptions[i].onclick = calcCart;
+    }
+});
 
-for(let i = 0; i < addItem.length; i++) {
-    addItem[i].addEventListener('click', addToCart)
+
+
+
+
+
+
+
+
+
+function calcCart() {
+    var cart = document.forms.cart;
+    var shoeIndex = cart.elements.shoe.selectedIndex;
+    var shoeCost = cart.elements.shoe.options[shoeIndex].value;
+    cart.elements.shoeCost.value = shoeCost
+    var shipCost = document.querySelector('input[name="shipping"]:checked').value;
+    cart.elements.shippingCost.value = formatNumber(shipCost);
+    var iIndex = cart.elements.insurance.selectedIndex;
+    var iCost = cart.elements.insurance.options[iIndex].value;
+    var totalCost = shoeCost + shipCost + iCost;
+    cart.elements.totalCost.value = formatUSCurrency(totalCost);
+
+
+
 }
 
-function addToCart(event){
-   let btn = event.target
-   let btn_parent = btn.parentElement
-   let btn_grandparent = btn.parentElement.parentElement
-   let itemName = btn_parent.children[1].innerText
-   let itemPrice =  btn_parent.children[2].innerText
-   let itemImage = btn_grandparent.children[0].src
 
-   let itemContainer = document.createElement('tr')
-   itemContainer.innerHTML = `<td><p>Shoe: ${itemName}  ${itemPrice}</p></td>`
-   console.log(itemName)
+function formatNumber(val, decimals) {
+    return val.toLocaleString(undefined, {
+        minimumFractionDigits: decimals,
+        maximumFractionDigits: decimals
+    });
 }
+
+function formatUSCurrency(val) {
+    return val.toLocaleString('en-US', { style: "currency", currency: "USD" });
+ }
